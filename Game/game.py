aa9fa31fe3b9f315864a510df488ac3652759175
Jack <jtam64@my.bcit.ca>
2022-11-22 08:48:09 -0800

    Turned main game into a class

--- Game/game.py
+++ Game/game.py
@@ -1,37 +1,44 @@
 import pygame
 
+class Game:
+    def __init__(self):
+        pass
+    
+    def game(self):
+        # Initialize game and set screen
+        pygame.init
+        window = pygame.display.set_mode((1500, 800))
+        running = True
+        clock = pygame.time.Clock()
+        # import background image and begin scrolling
+        bg = pygame.image.load("sprites/bg.png")
+        bg = pygame.transform.scale(bg, (3000, 800))
+        i = 0
+
+        while running:
+            # make background scroll
+            window.fill((0,0,0))
+            window.blit(bg, (i, 0))
+            window.blit(bg, (3000+i, 0))
+            # when background reaches the end, reset background
+            if i == -3000:
+                window.blit(bg, (3000+i, 0))
+                i = 0
+            # scroll background by number
+            i -= 3
+
+            clock.tick(30)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+                    quit()
+
+            pygame.display.update()
 
 def main():
-
-    pygame.init
-    window = pygame.display.set_mode((1500, 800))
-    running = True
-    clock = pygame.time.Clock()
-    # import background image and begin scrolling
-    bg = pygame.image.load("sprites/bg.png")
-    bg = pygame.transform.scale(bg, (3000, 800))
-    i = 0
-
-    while running:
-        # make background scroll
-        window.fill((0,0,0))
-        window.blit(bg, (i, 0))
-        window.blit(bg, (3000+i, 0))
-        # when background reaches the end, reset background
-        if i == -3000:
-            window.blit(bg, (3000+i, 0))
-            i = 0
-        # scroll background by number
-        i -= 10
-
-        clock.tick(30)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                quit()
-
-        pygame.display.update()
+    g = Game()
+    g.game()
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Pekin Snack
 Pygame for 2520
 
-> Eater face chasing a duck
-> Duck must fly away and get hoisin sauce
-> Hoisin sauce speeds you up
+> Old man face chasing a duck
+> Duck must fly away and get seeds
+> Seeds speeds you up
 > Kites slow you down
 > #### Reach the end and poop on the eater
