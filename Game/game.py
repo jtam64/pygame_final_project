4887f7913a05b5485d0fee7222bf863372d57119
Jack <jtam64@my.bcit.ca>
2022-11-22 08:27:39 -0800

    Added scrolling background

--- Game/game.py
+++ Game/game.py
@@ -4,14 +4,34 @@ import pygame
 def main():
 
     pygame.init
-    window = pygame.display.set_mode((500, 500))
+    window = pygame.display.set_mode((1500, 800))
     running = True
+    clock = pygame.time.Clock()
+    # import background image and begin scrolling
+    bg = pygame.image.load("sprites/bg.png")
+    bg = pygame.transform.scale(bg, (3000, 800))
+    i = 0
 
     while running:
+        # make background scroll
+        window.fill((0,0,0))
+        window.blit(bg, (i, 0))
+        window.blit(bg, (3000+i, 0))
+        # when background reaches the end, reset background
+        if i == -3000:
+            window.blit(bg, (3000+i, 0))
+            i = 0
+        # scroll background by number
+        i -= 10
+
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                pygame.quit()
+                quit()
 
         pygame.display.update()
+
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# pygame_final_project
+# Pekin Snack
 Pygame for 2520
+
+> Eater face chasing a duck
+> Duck must fly away and get hoisin sauce
+> Hoisin sauce speeds you up
+> Kites slow you down
+> #### Reach the end and poop on the eater
